2393616b5909fee4269e554d1cf34491b61f555b
tinaliu27 <27tinaliu@gmail.com>
2024-02-09 13:17:26 +0800

    added level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+       //  Scanner sc = new Scanner(System.in);
+        // ArrayList<Task> tasksList = new ArrayList<Task>(100);
+        String lineseperator = "----------------------------\n";
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
+                + "| \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "\n" + lineseperator);
+
+        String greet = "Hello! I'm Arcadia\nWhat can I do for you?\n" + lineseperator;
+        System.out.println(greet);
     }
 }
