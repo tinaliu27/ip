05d2faee1fdee1c5b0e6ab5c3e41779e901b9dde
tinaliu27 <27tinaliu@gmail.com>
2024-02-13 17:34:02 +0800

    added level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,15 +5,15 @@ public class Duke {
     public static void main(String[] args) {
        //  Scanner sc = new Scanner(System.in);
         // ArrayList<Task> tasksList = new ArrayList<Task>(100);
-        String lineseperator = "----------------------------\n";
+        String LINESEPERATOR = "----------------------------";
         String logo = " ____        _        \n"
                 + "| \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo + "\n" + lineseperator);
+        System.out.println("Hello from\n" + logo + "\n" + LINESEPERATOR);
 
-        String greet = "Hello! I'm Arcadia\nWhat can I do for you?\n" + lineseperator;
+        String greet = "Hello! I'm Arcadia\nWhat can I do for you?\n" + LINESEPERATOR;
         System.out.println(greet);
     }
 }
